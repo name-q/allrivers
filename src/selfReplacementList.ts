69d8ebe35fa309131a08bca14d5b89591d10e06f
name-q <1620206666@qq.com>
2023-03-17 16:06:36 +0800

    自替换List

---
+++ src/selfReplacementList.ts
@@ -0,0 +1,41 @@
+import { List, is } from "immutable";
+import { isArray, echo, isObject, isNumber } from "./util";
+
+interface selfRootList {
+  // 自替换系数 - 决定selfReplacementList.size
+  n: number;
+  // 自替换List
+  selfReplacementList: List<any>;
+  // 最新值
+  target: unknown;
+}
+
+/**
+ * 获取自替换List
+ */
+const getSelfReplacementList: echo<List<any>, selfRootList> = (value) => {
+  if (!isObject(value)) return List([]);
+  let { n, selfReplacementList, target } = value;
+  if (!isArray(selfReplacementList)) return List([]);
+  n = isNumber(n) && n > 0 ? n : 2;
+  let { size } = selfReplacementList;
+
+  // 比较数据 - 有变化再添加
+  let lastElement = selfReplacementList.last();
+  if (!is(lastElement, target)) {
+    selfReplacementList.push(target);
+    // 应用系数n
+    if (size === n) {
+      // 自替换流程
+      return selfReplacementList.shift();
+    } else if (size < n) {
+      // 记录流程
+      return selfReplacementList;
+    } else {
+      // 截取流程
+      return selfReplacementList.slice(-n);
+    }
+  } else {
+    return selfReplacementList;
+  }
+};
