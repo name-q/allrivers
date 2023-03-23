15e4e0c799aa95e3241738e0709e27662f682b8a
name-q <1620206666@qq.com>
2023-03-23 16:20:47 +0800

    清除数据方案

--- src/itemStorage.ts
+++ src/itemStorage.ts
@@ -3,26 +3,47 @@
 import { fromJS, Map, List } from "immutable";
 import { getSelfReplacementList } from "./selfReplacementList";
 
-type ItemStorage = Map<string, List<any> | number>;
+type ItemStorage = Map<string, List<any> | number | undefined>;
 
 class createItemStorage {
   private data: ItemStorage;
 
-  constructor(value: unknown = {}, n: number = 2) {
+  constructor(userValue: unknown = {}, n: number = 2) {
+    let value = getSelfReplacementList({
+      n,
+      selfReplacementList: List([]),
+      target: fromJS(userValue),
+    });
+
     this.data = Map({
-      value: getSelfReplacementList({
-        n,
-        selfReplacementList: List([]),
-        target: fromJS(value),
-      }),
+      value,
+      init: value,
       n,
     });
   }
 
   // TODO 对于数据处理的公共方案 结合mittx
 
-  public changeData():boolean {
-    return true
+  /**
+   * 清除数据 - 清除数据是将数据重置为初始值并不是卸载数据
+   * 同时会将数据绑定的一切方法初始化
+   * @returns boolean
+   */
+  public clearData(): boolean {
+    try {
+      this.data.set("value", this.data.get("init"));
+      // TODO clear mittxfunction
+      return true;
+    } catch {}
+    return false;
+  }
+
+  public changeData(): boolean {
+    try {
+      // TODO change function
+      return true;
+    } catch {}
+    return false;
   }
 
   public getData(): ItemStorage {
